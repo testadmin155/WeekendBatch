5ea78e178cbc1748316f3d90526bdd2537175cd6
Anees <skanees007@gmail.com>
2023-01-22 22:58:48 +0100

    Arrays and Forloop

--- JavaBasics/src/FirstPackage/Split.java
+++ JavaBasics/src/FirstPackage/Split.java
@@ -3,8 +3,22 @@ package FirstPackage;
 public class Split {
 
 	public static void main(String[] args) {
-		System.out.println("Split");
-		System.out.println("Split code");
+	
+		 String textmessage="Automation testing session for Selenium";
+		 
+		 System.out.println(textmessage.length());
+		String splitvalue[] =  textmessage.split("for");
+		 //String splitvalue[] =  textmessage.split(" ");
+		System.out.println(splitvalue.length);
+		System.out.println(splitvalue[0]);
+		System.out.println(splitvalue[1]);
+		//System.out.println(splitvalue[2]);
+		//System.out.println(splitvalue[4]);
+		//System.out.println(splitvalue[3]);
+		
+		//Normal Variable - can save only one value
+		// arrays - we want to save multiple values 
+		// array will save values from 0
 
 	}
 
