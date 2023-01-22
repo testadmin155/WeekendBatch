820e991edc67585d7cd07416cd212051c89e4a0c
Anees <skanees007@gmail.com>
2023-01-22 21:40:50 +0100

    MyFirstProjectCode

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/6f/40cfd0fed399001d1b0ed11dfa9a8be3
@@ -0,0 +1,5 @@
+package SecondPackage;
+
+public class MySecondProgram {
+
+}
---
+++ JavaBasics/src/SecondPackage/MySecondProgram.java
@@ -0,0 +1,10 @@
+package SecondPackage;
+
+public class MySecondProgram {
+
+	System.out.println("Hello");
+	System.out.println("Team");
+	System.out.println("how are you");
+	System.out.println("Hello");
+	System.out.println("Team");
+}
