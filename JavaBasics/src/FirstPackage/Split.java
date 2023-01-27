a4a57a7902a7f0c6dca9d02efc407bc6524e6cca
Anees <skanees007@gmail.com>
2023-01-27 23:05:21 +0100

    SecondWeek Friday class notes

--- JavaBasics/src/FirstPackage/Split.java
+++ JavaBasics/src/FirstPackage/Split.java
@@ -3,7 +3,6 @@ package FirstPackage;
 public class Split {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 	
 		 String textmessage="Automation testing session for Selenium";
 		 
@@ -20,9 +19,7 @@ public class Split {
 		//Normal Variable - can save only one value
 		// arrays - we want to save multiple values 
 		// array will save values from 0
-=======
-		System.out.println("Split");
->>>>>>> 3e317e4d202859d7ee0a5f4eeb60d71a5fe28d3e
+
 
 	}
 
---
+++ JavaBasics/src/FirstPackage/Switch.java
@@ -0,0 +1,37 @@
+package FirstPackage;
+
+public class Switch {
+
+	public static void main(String[] args) {
+		
+		String name ="Achala";
+		
+		switch(name)
+		{
+		case "Qasim":
+			System.out.println("I will talk to Qasim");
+			break;
+			
+		case "Usman":
+			System.out.println("I will talk to Usman");
+			break;
+			
+		case "Achala":
+			System.out.println("I will talk to Achala");
+			break;
+			
+		case "Suhaib":
+			System.out.println("I will talk to Suhaib");
+			break;
+			
+		case "Ashwini":
+			System.out.println("I will talk to Ashwini");
+			break;
+		
+		default:
+			System.out.println("Sleep");
+
+		}
+	}
+
+}
