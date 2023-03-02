044f64bc47ff9530e7c4e85a2905c353e6f92a65
Anees <skanees007@gmail.com>
2023-03-02 23:27:43 +0100

    Login MeEthod and STatic Dropdown

---
+++ SeleniumProject/src/Selenium/LoginMultiUsers.java
@@ -0,0 +1,79 @@
+package Selenium;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginMultiUsers {
+	
+	static WebDriver driver;
+	
+	static void Login(String username, String password)
+	{
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		driver.findElement(By.id("login1")).sendKeys(username);
+		driver.findElement(By.name("passwd")).sendKeys(password);
+		driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+		
+		String msg = driver.findElement(By.id("div_login_error")).getText();
+		System.out.println(msg);
+		
+		if (msg.equals("Wrong username and password combination."))
+		{
+			System.out.println("Pass");
+		}
+		else
+		{
+			System.out.println("Fail");
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Testing Session\\SeleniumTraining\\BrowserDrivers\\ChromeLatest\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		Login("Anees", "Test@1234");
+		Thread.sleep(3000);
+		
+		Login("Usman", "Password@1234");
+		Thread.sleep(3000);
+		
+		Login("Ashwini", "Testing");
+		Thread.sleep(3000);
+		
+		Login("Acahala", "Test@987");
+		Thread.sleep(3000);
+		
+		Login("Shoaib", "Password");
+		Thread.sleep(3000);
+		
+		
+		/*
+		 In Website what are the fields you see
+		 
+		 1. Edit Field/Text Field
+		 2. Radio button
+		 3. CheckBoxes
+		 4. Dropdowns
+		 5. Buttons
+		 6. Mousehover
+		 7. Calenders
+		 
+		 
+		 
+		 */
+		
+		// <a href="/gp/help/customer/display.html/ref=ap_signin_notification_condition_of_use?ie=UTF8&amp;nodeId=200545940" xpath="1">Conditions of Use</a>
+		
+		//Xpath - //a[text()='Conditions of Use']
+		
+		
+
+	}
+
+}
---
+++ SeleniumProject/src/Selenium/StaticDropdown.java
@@ -0,0 +1,74 @@
+package Selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class StaticDropdown {
+	/*
+	 Dropdowns - There are two type of Dropdown 
+	 1. Static 
+	 2. Dynamic
+	 
+	 
+	 1. Static Dropdown:
+	 		- If u find any dropdown with fixed options then it is called as static dropdown
+	 		- If you find any dropdown with tagname Select,then Selenium has a class to identify element
+	 		- We need to create an object for Select class and use it
+	 		- If there is not Select tagname, then we can't use this Select class 
+	 
+	 */
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Testing Session\\SeleniumTraining\\BrowserDrivers\\ChromeLatest\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://book.spicejet.com/");
+		driver.manage().window().maximize();
+		
+		//Script to select currency from a static dropdown
+		
+		Select currencydropdown = new Select(driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchView_DropDownListCurrency")));
+		String displayvalue = currencydropdown.getFirstSelectedOption().getText();
+		System.out.println("Default selected value is : "+displayvalue);
+		
+		//currencydropdown.selectByValue("GBP");
+		//currencydropdown.selectByIndex(6);
+		currencydropdown.selectByVisibleText("SAR");
+		
+		String displayvalue1 = currencydropdown.getFirstSelectedOption().getText();
+		System.out.println("Selected value is : "+displayvalue1);
+		
+		//Script to select Number of Passengers
+		
+		//Script to click on Passenger field
+		String defaultvalue=driver.findElement(By.id("divpaxinfo")).getText();
+		System.out.println("Default value : "+defaultvalue);
+		driver.findElement(By.id("divpaxinfo")).click();
+		
+		//Script to select Adult
+		Select adult = new Select(driver.findElement(By.name("ControlGroupSearchView$AvailabilitySearchInputSearchView$DropDownListPassengerType_ADT")));
+		adult.selectByVisibleText("3");
+		String defaultvalue1=driver.findElement(By.id("divpaxinfo")).getText();
+		System.out.println("Selected value : "+defaultvalue1);
+		
+		//Script to select Child
+		Select child = new Select(driver.findElement(By.id("ControlGroupSearchView_AvailabilitySearchInputSearchView_DropDownListPassengerType_CHD")));
+		child.selectByValue("2");
+		String defaultvalue2=driver.findElement(By.id("divpaxinfo")).getText();
+		System.out.println("Selected value : "+defaultvalue2);
+		
+		//Script to select Infant
+		Select infant = new Select(driver.findElement(By.xpath("(//select[contains(@class,'wMed1s')])[3]")));
+		infant.selectByIndex(1);
+		String defaultvalue3=driver.findElement(By.id("divpaxinfo")).getText();
+		System.out.println("Selected value : "+defaultvalue3);
+		
+		driver.findElement(By.id("divpaxinfo")).click();
+		driver.close();
+
+	}
+
+}
